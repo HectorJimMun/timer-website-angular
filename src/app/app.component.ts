06dd0123c04638c69dd78601a554335a11ec8871
Hector Jimenez <>
2025-02-07 10:58:20 +0100

    Organised logic to use different pages with Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Timer } from './models/timer.model';
 import { TimerListComponent } from "./components/timer-list/timer-list.component";
@@ -9,14 +9,25 @@ import { ClockComponent } from './components/clock/clock.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, TimerListComponent, ClockComponent],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-    private _timerService = inject(TimerServiceService);
-    timerList: Timer[] = [];
+export class AppComponent {
+    //private _timerService = inject(TimerServiceService);
+    //timerList: Timer[] = [];
 
+    menuOption: string = 'home';
+
+    /**
+     * Updates the current active menu option on the navigation bar.
+     * @param {string }menuOption Menu option activated.
+     */
+    onOption(menuOption: string) {
+        this.menuOption = menuOption;
+    }
+
+    /*
     ngOnInit(): void {
         this.timerList = this._timerService.getTimers();
     }
@@ -26,19 +37,23 @@ export class AppComponent implements OnInit {
         this._timerService.createTimer();
         this.timerList = this._timerService.getTimers();
     }
+    */
 
     /**
      * Calls the service to remove the timer.
      * @param {number} timerId ID of the timer to be removed.
-     */
+
     removeTimer(timerId: number): void {
         this._timerService.removeTimer(timerId);
         this.ngOnInit();
     }
+     */
 
+    /*
     // Calls the service to remove all the timers
     removeAllTimers(): void {
         this._timerService.removeAllTimers();
         this.ngOnInit();
     }
+    */
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ClockComponent } from './components/clock/clock.component';
+import { TimerListComponent } from './components/timer-list/timer-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ClockComponent},
+    {path: 'timers', component: TimerListComponent},
+    {path: '**', redirectTo: '', pathMatch: 'full'}
+];
